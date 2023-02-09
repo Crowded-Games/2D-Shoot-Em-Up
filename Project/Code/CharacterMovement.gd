aef51bbd2171a3964bd83001d4f7c83bd165faa6
Cataclock <Stevenson2628@gmail.com>
2023-02-09 13:00:01 -0800

    New Fire Rate function. And that is all for today

--- Project/Code/CharacterMovement.gd
+++ Project/Code/CharacterMovement.gd
@@ -3,18 +3,20 @@ extends CharacterBody2D
 const SPEED = 300.0
 @export var Bullet: PackedScene
 @onready var Camera = get_node("Camera2D")
+@export var fire_rate = 0.2
+var timer = 0
 
 func _ready():
 	# I have no idea why this makes the camera do that thing, but this is cool!
 	get_node("Camera2D").set("position", Vector2(100, 0))
 
-func _physics_process(_delta):
-
+func _physics_process(delta):
+	timer += delta
 	# Get the input direction and handle the movement/deceleration.
 	var direction_x = Input.get_axis("Left", "Right")
 	var direction_y = Input.get_axis("Up", "Down")
 	
-	if Input.get_action_raw_strength("Shoot"):
+	if Input.get_action_raw_strength("Shoot") && timer >= fire_rate:
 		var temp = Bullet.instantiate()
 		add_sibling(temp)
 		temp.global_position = get_node("BulletSpawn").get("global_position")
@@ -22,6 +24,7 @@ func _physics_process(_delta):
 		temp.set("area_direction", (get_global_mouse_position() - self.global_position).normalized())
 		# These statements below handle camera shake
 		get_node("Camera2D").set("offset", Vector2(randf_range(-4, 4), randf_range(-4, 4)))
+		timer = 0
 	else:
 		get_node("Camera2D").set("offset", Vector2(0, 0))
 	
