9d7a83bbcfaceef0e20ab7bd886d65171a717437
Cataclock <Stevenson2628@gmail.com>
2023-02-15 14:44:42 -0800

    small change

--- Project/Code/CharacterMovement.gd
+++ Project/Code/CharacterMovement.gd
@@ -10,7 +10,7 @@ var die: bool = false
 
 func _ready():
 	# I have no idea why this makes the camera do that thing, but this is cool!
-	get_node("Camera2D").set("position", Vector2(100, 0))
+	Camera.set("position", Vector2(100, 0))
 
 func _physics_process(delta):
 	timer += delta
@@ -32,10 +32,10 @@ func _physics_process(delta):
 		# this sets the rotation as to where it will fire
 		temp.set("area_direction", (get_global_mouse_position() - self.global_position).normalized())
 		# These statements below handle camera shake
-		get_node("Camera2D").set("offset", Vector2(randf_range(-4, 4), randf_range(-4, 4)))
+		Camera.set("offset", Vector2(randf_range(-4, 4), randf_range(-4, 4)))
 		timer = 0
 	else:
-		get_node("Camera2D").set("offset", Vector2(0, 0))
+		Camera.set("offset", Vector2(0, 0))
 	# movement is handled like this
 	if direction_x:
 		velocity.x = direction_x * SPEED
@@ -52,5 +52,5 @@ func Die():
 	get_node("Explosive").set_emitting(true)
 	get_node("Explosive/Sound").play()
 	self.get_node("MeshInstance2D").set("visible", false)
-	get_node("Camera2D").set("position", Vector2(0, 0))
+	Camera.set("position", Vector2(0, 0))
 	die = true
